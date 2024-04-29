71ddc889eb7b378c536fd77189be6f0d20bc0c31
jordan.whiteley <jordan.whiteley@veeva.com>
2024-04-29 14:16:57 -0700

    added functionality for weekly matchups

---
+++ utils/leagueInfo.js
@@ -0,0 +1,63 @@
+export const leagueID = "1048770475687571456";
+export const leagueName = "Dynasty Is Crazy Cool (DICC)";
+export const dynasty = true;
+export const last_regular_season_week = 14;
+
+export const homepageText = `
+    <h1>Dynasty Is Crazy Cool</h1>
+    <br>
+    <p>League Test Page</p>
+`;
+
+export const managers = [
+    {
+        "managerID": "471440855413026816",
+        "name": "DynastyRyan",
+        "favoriteTeam": "sf"
+    },
+    {
+        "managerID": "860226270301097984",
+        "name": "tickler69",
+        "favoriteTeam": "dal"
+    },
+    {
+        "managerID": "960320213864501248",
+        "name": "cjharrington",
+        "favoriteTeam": "chi"
+    },
+    {
+        "managerID": "1000170433901559808",
+        "name": "SicBeats",
+        "favoriteTeam": "dal"
+    },
+    {
+        "managerID": "1039669220303089664",
+        "name": "sirauthorv",
+        "favoriteTeam": "dal"
+    },
+    {
+        "managerID": "1049234670099095552",
+        "name": "stephenASingh",
+        "favoriteTeam": "pit"
+    },
+    {
+        "managerID": "1049488714805821440",
+        "name": "TheBigDuck",
+        "favoriteTeam": "chi"
+    },
+    {
+        "managerID": "1049869600781217792",
+        "name": "rushbal",
+        "favoriteTeam": "sf"
+    },
+    {
+        "managerID": "1050051315855945728",
+        "name": "arjmenon",
+        "favoriteTeam": "ind"
+    },
+    {
+        "managerID": "1050276922992766976",
+        "name": "Hinroski",
+        "favoriteTeam": "dal"
+    },
+]
---
+++ utils/weekDropdown.js
@@ -0,0 +1,36 @@
+import { last_regular_season_week, leagueID } from "./leagueInfo";
+
+document.addEventListener('DOMContentLoaded', function() {
+    const weekDropdown = document.getElementById('week_num');
+
+    for (let i = 1; i <= last_regular_season_week; i++) {
+        const option = document.createElement('option');
+        option.value = i.toString();
+        option.text = 'Week ' + i;
+        weekDropdown.appendChild(option);
+    }
+
+    weekDropdown.addEventListener('change', function() {
+        getWeeklyMatchupData();
+    });
+});
+
+function getWeeklyMatchupData() {
+    const selectedWeek = document.getElementById('week_num').value;
+    const url = 'https://api.sleeper.app/v1/league/' + leagueID + '/matchups/' + selectedWeek;
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(data => {
+            document.getElementById('responseContainer').innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+            document.getElementById('responseContainer').innerHTML = 'Error: ' + error.message;
+        });
+}
