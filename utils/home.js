2dd0c7fbcd4e5159ce127a09b18d3567a6c591ef
jordan.whiteley <jordan.whiteley@veeva.com>
2024-05-01 12:20:15 -0700

    adding button and link to transaction page

--- utils/home.js
+++ utils/home.js
@@ -2,13 +2,23 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     const weeklyMatchupBtn = document.getElementById('week_match_btn');
+    const weeklyTransactionsBtn = document.getElementById('week_transaction_btn');
 
     weeklyMatchupBtn.addEventListener('click', function() {
         redirectWeeklyMatchupBtn();
+    });
+
+    weeklyTransactionsBtn.addEventListener('click', function() {
+        redirectWeeklyTransactionBtn();
     });
 });
 
 function redirectWeeklyMatchupBtn() {
     var weeklyMatchupPage = 'weekly_matchup.html';
     window.location.href = weeklyMatchupPage;
+}
+
+function redirectWeeklyTransactionBtn() {
+    var weeklyTransactionPage = 'weekly_transactions.html';
+    window.location.href = weeklyTransactionPage;
 }
