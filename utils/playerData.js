41517d66277abfe90ddd483bfb284250ed36f975
jordan.whiteley <jordan.whiteley@veeva.com>
2024-05-03 15:30:23 -0700

    testing player data

--- utils/playerData.js
+++ utils/playerData.js
@@ -1,11 +1,6 @@
 function getPlayerData(playerId) {
     return fetch('./data_files/playerInfo.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => data[playerId])
         .catch(error => {
             console.error('Fetch error: ', error);
